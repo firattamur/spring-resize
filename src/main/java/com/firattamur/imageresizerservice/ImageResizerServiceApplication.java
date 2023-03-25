f2d2fc65f786b122dc0e0e63e9daa562fb6f4f31
firattamur <ftamur16@ku.edu.tr>
2023-03-25 04:22:34 +0300

    :wrench: chore: initial project structure and springdoc

---
+++ src/main/java/com/firattamur/imageresizerservice/ImageResizerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.firattamur.imageresizerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageResizerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageResizerServiceApplication.class, args);
+    }
+
+}
